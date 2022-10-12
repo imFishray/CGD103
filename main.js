1f5bf6442c55645b43b223ab72a35e663b9c8f67
imFishray <yulielove0530@gmail.com>
2022-10-13 01:48:55 +0800

    10/13

--- main.js
+++ main.js
@@ -1,21 +1,8 @@
-export const detectZoom = () => {
-    let ratio = 0,
-      screen = window.screen,
-      ua = navigator.userAgent.toLowerCase();
-    if (window.devicePixelRatio !== undefined) {
-      ratio = window.devicePixelRatio;
-    } else if (~ua.indexOf('msie')) {
-      if (screen.deviceXDPI && screen.logicalXDPI) {
-        ratio = screen.deviceXDPI / screen.logicalXDPI;
-      }
-    } else if (
-      window.outerWidth !== undefined &&
-      window.innerWidth !== undefined
-    ) {
-      ratio = window.outerWidth / window.innerWidth;
-    }
-    if (ratio) {
-      ratio = Math.round(ratio * 100);
-    }
-    return ratio;
-};
+/* 漢堡包開關 */
+function init(){
+  document.getElementById("Burger_Button").onclick=function(){
+    let menu = document.getElementById("Nav-Item");
+    menu.classList.toggle("hidden");
+  }
+}
+window.addEventListener("load",init,false)
