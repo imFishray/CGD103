b4d2a2ce6c7ee032d0159e060387b0d86d194417
imFishray <yulielove0530@gmail.com>
2022-10-25 04:04:25 +0800

    10/25

--- main.js
+++ main.js
@@ -5,5 +5,17 @@ function init(){
     menu.classList.toggle("hidden");
   }
 }
+/* 點小圖換大圖 */
+function clickchangeimg(){
+  var list = document.getElementsByClassName("littlepic")
+  var bigpic = document.getElementsByClassName("bigpic")
+  for(var i = 0;i<list.length;i++){
+    console.log(i);
+    list[i].onclick = function(){
+      bigpic[0].src = this.src;
+    }
+  }
+}
+
 window.addEventListener("load",init,false)
 
